7b6f79f420aedf5bd2fc691d706f07c614d07d2d
Anil Madhavapeddy <anil@recoil.org>
2011-11-05 17:53:57 +0000

    update plotting script to generate pdf of xen/native

--- core2core.r
+++ core2core.r
@@ -1,11 +1,20 @@
-plot_fields <- function (test) {
-  g <- read.table(sprintf("collected/%s.csv", test))
+plot_fields <- function (subdir,test) {
+  g <- read.table(sprintf("archive/%s/%s.csv", subdir, test))
   m <- as.matrix(g)
   a <- mean(m)
-  for (i in 1:48) m[i,i] <- a
   image(m)
+  title(subdir, sub=sprintf("%s raw", test), cex.main=0.9)
+  for (i in 1:dim(m)[1])
+    m[i,i] <- a
+  image(m)
+  title(subdir, sub=sprintf("%s norm", test), cex.main=0.9)
 }
 
-par(mfrow=c(3,4))
+subdirs=c("native-48core-linux", "xen-48core-linux-dom0")
+
+pdf(file="core2core.pdf", paper="a4")
 tests=c("pipe_thr","pipe_lat","tcp_thr","tcp_lat","unix_thr","unix_lat","mempipe_thr")
-for (t in tests) plot_fields(t)
+for (s in subdirs)  {
+  par(mfrow=c(4,4))
+  for (t in tests) plot_fields(s,t)
+}
