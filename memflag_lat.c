236b14f97739d11f9bfb932736176ea8663ea40e
Anil Madhavapeddy <anil@recoil.org>
2011-11-08 12:31:30 +0000

    add licenses

--- memflag_lat.c
+++ memflag_lat.c
@@ -1,8 +1,36 @@
-/* Ulitimate in simple IPC protocols: establish a shared memory
+/* 
+    Measure latency of IPC using tcp sockets
+
+    Copyright (c) 2011 Steven Smith <sos22@cam.ac.uk>
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/* Ultimate in simple IPC protocols: establish a shared memory
    segment and then send a ping by setting flags in it, with the other
-   end spinning reading those flags. */
-/* Note that this is a straight up latency test: no actual data is
+   end spinning reading those flags.
+   Note that this is a straight up latency test: no actual data is
    transferred. */
+
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <err.h>
--- mempipe_thr.c
+++ mempipe_thr.c
@@ -1,3 +1,30 @@
+/* 
+    Measure latency of IPC using tcp sockets
+
+    Copyright (c) 2011 Steven Smith <sos22@cam.ac.uk>
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 /* Simple shared-memory IPC throughput test, with spin-waits rather
    than blocking.  The shared area is divided into packets, each of
    which starts with a header.  The header is the size of a cache
@@ -11,6 +38,7 @@
    message.  The benchmark which we build on top of this just keeps
    sending fixed-sized messages for soem number of times, waiting as
    appropriate for the receiver to pick them up. */
+
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/time.h>
