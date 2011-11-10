956f078223979d356546010f978b5e2ec3f9b856
Anil Madhavapeddy <anil@recoil.org>
2011-11-10 16:44:18 +0000

    add js viewer

--- core2core-collect.sh
+++ core2core-collect.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
-
-OUT=results
-COLLECT=collected
+set -e
+NAMES="native48 xen48nopin xen48pin"
+COLLECT=graph
 TESTS="pipe_thr pipe_lat tcp_thr tcp_lat unix_thr unix_lat mempipe_thr"
+NCORES=48
 
-rm -rf ${COLLECT}
-mkdir -p ${COLLECT}
+for NAME in ${NAMES}; do
+OUT=results.${NAME}
+idx="${COLLECT}/${NAME}_all.js"
+echo "if (!tests) tests={};" > ${idx}
 for t in ${TESTS}; do
-  for c1 in `jot 48 0`; do
-    for c2 in `jot 48 0`; do
+  name="${NAME}_${t}"
+  ofile=${COLLECT}/${name}.js
+  rm -f ${ofile}
+  echo "var ${name} = [" >> ${ofile}
+  for c1 in `jot ${NCORES} 0`; do
+    echo " [" >> ${ofile}
+    for c2 in `jot ${NCORES} 0`; do
       d="${OUT}/${c1}-${c2}-${t}/01-${t}-headline.log"
       speed=`tail -1 ${d} | awk '{print $4}' | sed -e 's/s//g' | sed -e 's/Mbp$//g'`
-      echo -n "${speed} " >> ${COLLECT}/${t}.csv
+      echo -n "${speed}," >> ${ofile}
     done
-    echo "" >> ${COLLECT}/${t}.csv
+    echo "]," >> ${ofile}
   done
+  echo "];" >> ${ofile}
+  cat ${ofile} >> ${idx}
+  echo "tests[\"${name}\"] = ${name};" >> ${idx}
+done
 done
