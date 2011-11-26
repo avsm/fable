556fc5569e2a5aa50e585066a0c43a5e58ef03d0
Anil Madhavapeddy <anil@recoil.org>
2011-11-26 00:14:07 +0000

    in TCP tests, spin for a successful connect(2) as well, and remove the sleep as it is not needed any more

--- tcp_lat.c
+++ tcp_lat.c
@@ -115,12 +115,11 @@ init_parent(test_data *td)
   ts->buf = xmalloc(td->size);
   struct addrinfo *res = ts->info;
   int sockfd;
-  sleep(1); 
   if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1)
     err(1, "socket");
     
-  if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1)
-    err(1, "connect");
+  while (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1)
+    { }
 
 #ifdef USE_NODELAY
   int flag = 1;
