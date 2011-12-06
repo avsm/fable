8e83c1b1d2f2d7897ac911c891b7e96ec409e2c5
Anil Madhavapeddy <anil@recoil.org>
2011-12-06 11:09:00 +0000

    add 2 scripts to run tests with "optimal" scheduling where all cores are communicating to one other local core, and a non-optimal version where they are scheduled to always be talking off core.

---
+++ core2core-all-optimal.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+SIZE="4096"
+COUNT=20000000
+TESTS="pipe_thr"
+
+ODIR="results.allthr.native.opt"
+rm -rf ${ODIR}
+mkdir -p ${ODIR}
+
+for base in {0..7..1}; do
+  echo -n ${base}: 
+  for c in {0..2..1}; do
+    c1=$(((${base} * 6) + (${c} * 2)))
+    c2=$((${c1}+1))
+    echo -n "${c1}x${c2} "
+    for t in ${TESTS}; do
+      d="${ODIR}/${SIZE}/${c1}-${c2}-${t}"
+      mkdir -p ${d}
+      ./${t} -s ${SIZE} -c ${COUNT} -a ${c1} -b ${c2} -o ${d} -m 2 &
+    done
+  done
+  echo
+done
+
+wait
