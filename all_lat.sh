a5575de02642a4dd06e1c9c7d88c2acfe408de1f
ms705 <ms705@cl.cam.ac.uk>
2012-01-22 19:36:15 +0000

    Fix latency data generation.

--- all_lat.sh
+++ all_lat.sh
@@ -5,16 +5,15 @@ COUNT=10000
 LAT_TESTS="pipe_lat tcp_lat unix_lat mempipe_lat tcp_nodelay_lat"
 TESTS="${LAT_TESTS}"
 
-MAX_COREID=$2
-echo "cores: ${MAX_COREID}"
+NUM_CORES=$2
 
 ODIR=$1
 rm -rf ${ODIR}
 mkdir -p ${ODIR}
 
 for SIZE in ${SIZES}; do
-for c1 in $(jot ${MAX_COREID}); do
-  for c2 in $(jot ${MAX_COREID}); do
+for c1 in $(jot ${NUM_CORES} 0); do
+  for c2 in $(jot ${NUM_CORES} 0); do
     echo ${c1} to ${c2}
     for t in ${TESTS}; do
       d="${ODIR}/${SIZE}/${c1}-${c2}-${t}"
@@ -25,14 +24,22 @@ for c1 in $(jot ${MAX_COREID}); do
         count=${COUNT}
       fi
       ./${t} -t -s ${SIZE} -c ${count} -a ${c1} -b ${c2} -o ${d}
-
-      # post-processing
-      ofile=${ODIR}/${t}.csv
-      d="${ODIR}/${SIZE}/${c1}-${c2}-${t}/01-${t}-headline.log"
-      speed=`tail -1 ${d} | awk '{print $4}' | sed -e 's/s//g'`
-      echo -n "${speed} " >> ${ofile}
-      rm ${ODIR}/${SIZE}/${c1}-${c2}-${t}/01-${t}-raw_tsc.log
     done
   done
 done
+
+# post-processing
+for t in ${TESTS}; do
+ofile=${ODIR}/${t}.csv
+for c1 in $(jot ${NUM_CORES} 0); do
+  for c2 in $(jot ${NUM_CORES} 0); do
+    d="${ODIR}/${SIZE}/${c1}-${c2}-${t}/01-${t}-headline.log"
+    speed=`tail -1 ${d} | awk '{print $4}' | sed -e 's/s//g'`
+    echo -n "${speed} " >> ${ofile}
+#    rm ${ODIR}/${SIZE}/${c1}-${c2}-${t}/01-${t}-raw_tsc.log
+  done
+  echo "" >> ${ofile}
+done
+done
+
 done
