bfa0278c181627afca6bccc30889c6607b27ea64
Malte Schwarzkopf <ms705@srcf.ucam.org>
2012-01-22 13:24:40 +0000

    Sortable table, some more data, refactor various bits.

--- web/processing/cron.sh
+++ web/processing/cron.sh
@@ -3,20 +3,23 @@
 CUR_DIR=$(pwd)
 
 RESULTS_DIR=../../results
-OUT_DIR=/usr/groups/netos/html/ipc-bench
-HTML_OUTFILE=index.html
+OUT_DIR=../public
+HTML_OUTFILE=results.html
 
 cd ${RESULTS_DIR}
 RESULTS=$(ls tmp*.tar.gz)
 cd ${CUR_DIR}
 
+# Generate results.html
 cat ${PWD}/html_head.tmpl > ${OUT_DIR}/${HTML_OUTFILE}
+cat ${PWD}/html_results_head.tmpl >> ${OUT_DIR}/${HTML_OUTFILE}
 for r in ${RESULTS}; do
   ARCHIVE=${r}
   echo -n "Extracting ${ARCHIVE}..."
   tar -xzf ${RESULTS_DIR}/${ARCHIVE}
   echo " done!"
-  DIR=tmp/$(echo ${r} | sed 's/\.tar\.gz//g')
+  NAME=$(echo ${r} | sed 's/\.tar\.gz//g')
+  DIR=tmp/${NAME}
   # Check the extracted data directory exists
   if [[ ! -d ${DIR} ]]; then
     echo "FATAL: After extraction, ${DIR} did not exist!"
@@ -26,12 +29,21 @@ for r in ${RESULTS}; do
   # Find the target cores used
   TARGET_CPUS=$(cat ${DIR}/logs/target_cpus)
 
-  # Now extract system information and results
-  python process.py ${DIR} ${OUT_DIR}/${HTML_OUTFILE} ${TARGET_CPUS}
+  # Now generate the results overview page
+  python gen-overview-entry.py ${DIR} ${OUT_DIR}/${HTML_OUTFILE} \
+    ${TARGET_CPUS} ${NAME}
+
+  # Generate details page
+#  python gen-details-page.py ${DIR} ${OUT_DIR} ${TARGET_CPUS} ${NAME}
+
+  # Move graphs
+  mkdir -p ${OUT_DIR}/graphs
+  rm -rf ${OUT_DIR}/graphs/${NAME}
+  mv ${DIR}/graphs ${OUT_DIR}/graphs/${NAME}
+  chmod -R g+rx ${OUT_DIR}/graphs/${NAME}
+  chmod -R o+rx ${OUT_DIR}/graphs/${NAME}
 done
+cat ${PWD}/html_results_foot.tmpl >> ${OUT_DIR}/${HTML_OUTFILE}
 cat ${PWD}/html_foot.tmpl >> ${OUT_DIR}/${HTML_OUTFILE}
 
-rm -rf ${OUT_DIR}/tmp
-mv tmp ${OUT_DIR}/
-chmod -R g+rx ${OUT_DIR}/tmp
-chmod -R o+rx ${OUT_DIR}/tmp
+
